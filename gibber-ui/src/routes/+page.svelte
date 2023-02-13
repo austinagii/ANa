4d4be0c3b3b5686dbf7feef2c8365e6a5c479a64
Kadeem Austin <dev@kadeemaustin.ai>
2023-02-13 18:24:56 -0400

    feat: add form for generating gibber text

--- gibber-ui/src/routes/+page.svelte
+++ gibber-ui/src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    #gibber-generate-form {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    #gibber-generate-form input[type="text"] {
+        margin-right: 10px;
+    }
+</style>
+
+<script>
+    function handleSubmit() {
+        console.log('submitted');    
+    }
+</script>
+
+<div>
+    <form on:submit|preventDefault={handleSubmit} id='gibber-generate-form'>
+        <input type="text"/>
+        <button type="submit">Submit</button>
+    </form>
+</div>
