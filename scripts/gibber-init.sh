93b1caa61ae97a5c0bf30d343294c726fc267ee2
Kadeem Austin <dev@kadeemaustin.ai>
2023-03-29 23:46:14 -0400

    refactor: rename project and add ana cli scripts

--- scripts/gibber-init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Install the required software
-apt install -y jq azure-cli
--- scripts/gibber-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ $VM_STATUS = "VM Stopped" ]] || [[ $VM_STATUS == "VM Deallocated" ]]; then
-    echo "Starting VM..."
-    az vm start -g $RESOURCE_GROUP -n $VM_NAME
-fi
