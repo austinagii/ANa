116318cb28c81e6183f3e0c447f06cf801faa40b
Kadeem Austin <kadeem@lagomxp.com>
2023-04-26 03:10:31 -0400

    feat(core.codec): accept generator as input to encode method

--- ana-core/core/codec.py
+++ ana-core/core/codec.py
@@ -1,7 +1,8 @@
 import torch 
 
 from collections.abc import Sequence
-from typing import TypeAlias
+from types import GeneratorType
+from typing import TypeAlias, Generator
 
 
 Token: TypeAlias = str
@@ -14,26 +15,41 @@ class TokenCodec:
         self._token_by_id: dict[int, Token] = {} # reverse mapping of ids to tokens for decoding
         self._next_id = 0
             
-    def encode(self, tokens: Sequence[Token]) -> list[int]:
-        """Returns the encoding of the provided tokens"""
-        encoded_tokens = torch.empty(len(tokens), dtype=torch.int32)
-        for i, token in enumerate(tokens):
-            # assign a unique id to each token only if one is not already assigned
-            if (token_id := self._id_by_token.get(token)) is None:
-                if self._frozen:
-                    raise OutOfVocabularyError(token)
-                
-                # assign the token the next available id 
-                token_id = self._next_id
-                self._id_by_token[token] = token_id
-                self._token_by_id[token_id] = token
-                # ensure that the next token has a different id
-                self._next_id += 1 
-            encoded_tokens[i] = token_id
-        return encoded_tokens
+    def encode(
+        self, 
+        tokens: Sequence[Token] | Generator[Token, None, None]
+    ) -> Sequence[int] | Generator[int, None, None]:
+        """Returns the encoding of the provided tokens
+        
+        The return type of this method depends on the type of the `tokens` argument.
+        If `tokens` is a sequence, a sequence of ints will be returned. If `tokens` is
+        a generator, a generator of ints will be returned. This allows for the encoding
+        to be streamed from disk rather than loaded into memory all at once.
+        """
+        match tokens:
+            case Sequence():
+                return [self._encode(token) for token in tokens]
+            case Generator():
+                return (self._encode(token) for token in tokens)
+            case _:
+                raise TypeError("Argument must be a sequence or generator")
+            
+    def _encode(self, token: Token) -> int:
+        """Returns the encoding of the provided token"""
+        # assign a unique id to each token only if one is not already assigned
+        if (token_id := self._id_by_token.get(token)) is None:
+            if self._frozen:
+                raise OutOfVocabularyError(token)
+            # assign the token the next available id 
+            token_id = self._next_id
+            self._id_by_token[token] = token_id
+            self._token_by_id[token_id] = token
+            # ensure that the next token has a different id
+            self._next_id += 1 
+        return token_id 
 
     def decode(self, encoded_text: Sequence[int]) -> str:
-        return "".join([self._token_by_id[_id.item()] for _id in encoded_text])
+        return "".join([self._token_by_id[_id] for _id in encoded_text])
 
     @property 
     def is_frozen(self):
@@ -44,6 +60,10 @@ class TokenCodec:
         if not isinstance(is_frozen, bool):
             raise ValueError("Argument must be a boolean value")
         self._frozen = is_frozen
+
+    @property
+    def vocabulary(self):
+        return set(self._id_by_token.keys())
 
 
 class OutOfVocabularyError(Exception):
