87caa0a12818f2d939466bfd7b642de9553a10bf
Kadeem Austin <dev@kadeemaustin.ai>
2023-05-01 13:36:32 -0400

    refactor(core): extract data loading from main method to 'DataLoader' class

--- ana-core/core/__main__.py
+++ ana-core/core/__main__.py
@@ -5,24 +5,12 @@ import argparse
 from pathlib import Path 
 
 import pandas as pd 
-from featuretoggles import TogglesList
 
+from core.language import DataLoader
 from core.language.models import NGramModel 
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger("ANa")
-
-def _load_training_corpus():
-        # load the dataset
-        dataset_path = Path(__file__).absolute().joinpath("../../data/text_emotion.csv").resolve()
-        log.info('Loading dataset from {dataset_path}')
-        if not dataset_path.exists():
-            log.fatal(f'The dataset was not found at the specified path: "{dataset_path}"')
-            sys.exit(-1)
-        else:
-            log.info('Dataset successfully loaded')
-        dataset = pd.read_csv(dataset_path)
-        return dataset['content'].str.cat(sep=' ')
 
 if __name__ == "__main__":
     # load the application configuration
@@ -32,7 +20,8 @@ if __name__ == "__main__":
     
     # create and train the model
     model = NGramModel(**vars(args))
-    model.train(_load_training_corpus())
+    dataloader = DataLoader()
+    model.train(dataloader.load_training_corpus())
     # respond to the user's prompts until they wish to stop
     while (prompt := input('Give me a prompt: ')) != 'bye':
         print(model(prompt))
--- ana-core/core/language/__init__.py
+++ ana-core/core/language/__init__.py
@@ -1,3 +1,4 @@
 from .codec import TokenCodec
+from .dataloader import DataLoader
 from .reader import NGramReader
 from .tokenizer import Tokenizer
