a763cb88ce580696fb378895ae235457ee0455d6
Kadeem Austin <dev@kadeemaustin.ai>
2023-05-16 20:35:21 -0400

    refactor(cli): reorganize code to reduce conditional nesting

--- ana-cli/init.sh
+++ ana-cli/init.sh
@@ -1,32 +1,48 @@
 #!/usr/bin/env bash
 
-# Add the main 'ana' script to the user's path
-if which ana >/dev/null; then
-    echo "Command 'ana' already exists on PATH at '$(which ana)'"
-    while true; do
-        echo "Would you like to delete it? [Y/n]"
-        read RESPONSE
-        case $RESPONSE in
-            Y)
-                sudo rm -f $(which ana)
-                if [ $? -eq 0 ]; then
-                    echo "File removed successfully"
-                fi
-                break
-                ;;
-            n)
-                # Exit with an error code if the exisitng 'command' is not a symlink to the 'ana.sh' script
-                if [ $(realpath $(which ana)) != "$(realpath $0)/cli/ana.sh" ]; then
-                    echo "The existing 'ana' does not reference the ana script located in the CLI"
+# Get the path to the 'ana' command if present on the executable path
+ANA_PATH=$(which ana)
+ANA_CLI_SCRIPT_PATH=$(dirname $(realpath $0))/cli/ana.sh
+
+if [ -n $ANA_PATH ] && [ "$(realpath "$ANA_PATH")" == "$ANA_CLI_SCRIPT_PATH" ]; then
+    # Do nothing if the 'ana' command already exists on the executable path and is linked to the main ANa CLI script.
+    echo "Command 'ana' already exists and is linked correctly"
+else
+    # Prompt the user to remove the existing 'ana' command if it is not linked to the main ANa CLI script.
+    if [ -n $ANA_PATH ]; then
+        echo "Command 'ana' already exists on PATH at '$ANA_PATH' but is not linked to the main ANa CLI script"
+        while true; do
+            echo "Would you like to delete the existing command and re-link it? (Y/n)"
+            read RESPONSE
+            case $RESPONSE in
+                Y)
+                    rm -f $ANA_PATH &>/dev/null
+                    if [ $? -eq 0 ]; then
+                        echo "Existing command removed successfully"
+                    else
+                        echo "Failed to remove the existing command. Ensure that you are executing this script with"\
+                                "'sudo'" >/dev/stderr
+                        exit 1
+                    fi
+                    break
+                    ;;
+                n)
                     exit 1
-                fi
-                ;;
-            *)
-                echo "Not a valid option"
-                ;;
-        esac
-    done
+                    ;;
+                *)
+                    ;;
+            esac
+        done
+    fi
+
+    # Add the 'ana' command if it does not already exist on the executable path. 
+    sudo -n ln -s $ANA_CLI_SCRIPT_PATH /usr/local/bin/ana &>/dev/null
+    if [ $? -ne 0 ]; then
+        echo "Could not create and link the 'ana' command. Ensure that you are executing this script with"\
+                "'sudo'" >/dev/stderr
+        exit 1
+    fi
 fi
 
-# create a symlink in the user's bin to the ana command
-sudo ln -s $(dirname $(realpath $0))/cli/ana.sh /usr/local/bin/ana
+
+
